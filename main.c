56c6ef80b023041915a9fc86f89c0e7dfbc5e1d5
Kislitsin Denis <dkislitzin@gmail.com>
2019-04-15 20:49:14 +0700

    Create function showdesk

---
+++ main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+void showdesk(char desk[8][8])
+
+{
+    int i, j;
+
+    printf("\n");
+
+    for (i = 7; i >= 0; i--) {
+        printf("%d|", i + 1);
+
+        for (j = 0; j < 8; j++) {
+            printf(" %c", desk[i][j]);
+        }
+        printf("\n");
+    }
+    printf("   _______________\n");
+    printf("   A B C D E F G H\n");
+}
+
+int main()
+{
+    char desk[8][8] = {"rhbqkbhr",
+                       "pppppppp",
+                       "........",
+                       "........",
+                       "........",
+                       "........",
+                       "PPPPPPPP",
+                       "RHBQKBHR"};
+    showdesk(desk);
+    return 0;
+}
